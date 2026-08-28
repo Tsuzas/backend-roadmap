2152930b7c1da40f5b054dc7ad0078f13dff6bae
Tsuzas <f.pereira.2002@hotmail.com>
2026-08-28 03:00:20 +0100

    feat(main.py): - Summary especific overview is operational - Delete  via Id is operational with Id readjust per deletion to kepe it intuitive to user - Print list was properly adjusted via forcefully spacing

--- begginer/expense-tracker/main.py
+++ begginer/expense-tracker/main.py
@@ -20,7 +20,8 @@ def loadExpense():
 def saveExpense(data, expenses):
     try:
         with open("expenses.json", "w") as file:
-            expenses.append(data)
+            if data != None:
+                expenses.append(data)
             json.dump(expenses, file, indent = 4)
 
     except FileNotFoundError:
@@ -53,13 +54,20 @@ list_parser = subparsers.add_parser('list' ,help='Shows whole list of expenses')
 
 # Summarty subparser, shows summary of spendage
 summary_parser = subparsers.add_parser('summary' ,help = "Shows the spendage across all months, use --month Y, for further details.")
-summary_parser.add_argument("--month" ,help = "For a more in depth analys of summary.")
+summary_parser.add_argument("--month" ,help = "For a more in depth analys of summary.", type = int)
+
+# Delete subparser, deletes certain grocery
+delete_parser = subparsers.add_parser('delete', help ="Remove a certain expense, usage:. python <program> delete [number]")
+delete_parser.add_argument("id", type = int)
+
 
 #No idea what this does either
 args = parser.parse_args()
 
 if (args.command) == "list":
-    print(expenses)
+    print(f"{"ID":<8}{"Date":<16}{"Description":<20}Amount")
+    for expense in expenses:
+        print(f"{expense["id"]:<8}{expense["date"][0:10]:<16}{expense["description"]:<20}{expense["amount"]}")
 elif (args.command) == "add":
     # Prints for debugging
     print(args.description)
@@ -75,18 +83,45 @@ elif (args.command) == "add":
 
     saveExpense(data, expenses)
 elif (args.command) == "summary":
+
+    # Initializes Total for calculations
     total = 0
+
+    # If month argument is not used it will always return None, so using if not null is a good way to use in case theres a month to check first
+
+    if (args.month)!= None:
+        print(args.month)
+        for expense in expenses:
+            if expense["date"][5:7] == args.month or expense["date"][5:7] == "0" + str(args.month):
+                total += expense["amount"]
+        print(f"\nTotal Spent: {round(total,2)}€\n")
+
+        # Exits early to avoid the general summary
+        exit()
+
     for expense in expenses:
         total += expense["amount"]
 
-    print(f"\nTotal Spent: {total}€\n")
+    print(f"\nTotal Spent: {round(total,2)}€\n")
+
+elif (args.command) == "delete":
+    print(f"working? {args.command} and {args.id}")
+
+    # Data as None so n Save expenses it can fail check and proceed
+    data = None
+
+    #Remove dic associated with ID
+    del expenses[args.id - 1]
+    for i, expense in enumerate(expenses, start=1):
+        expense["id"] = i
+    saveExpense(data, expenses)
 
 # TODO 
 # XXX DYNAMIZE ID,
 # XXX FIX OVERWRITE FOMR SAVEFUNCTUION
 # XXX SUMMARY, 
-# SUMMARY MONTH SPECIFIC, 
-# DELETE ID,
-# REFINE PRINT LIST,
+# XXX SUMMARY MONTH SPECIFIC, 
+# XXX DELETE ID,
+# XXX REFINE PRINT LIST,
 # ERROR HANDLE IMPROPER COMMANDS,
 # MODULARISE WITH FUNCTIONS, SAME FILE THO I AINT CREAITNG MORE FILES FOR THIS SIZE, NVM JUST CHEKCED THIS IS GETTING KIND ABIG 
